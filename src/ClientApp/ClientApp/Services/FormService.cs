e82699d2857b4a686b017a96012a22fc74f39703
Pavel Antropov <p.m.antropov@gmail.com>
2022-05-15 23:09:58 +0400

    Move logic from the view to a service

---
+++ src/ClientApp/ClientApp/Services/FormService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using ClientApp.Models;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace ClientApp.Services
+{
+    public static class FormService
+    {
+        public static Form LoadForm(byte[] bytes)
+        {
+            using var stream = new MemoryStream(bytes);
+            using var reader = new StreamReader(stream);
+
+            var json = reader.ReadToEnd();
+
+            var data = JsonConvert.DeserializeObject<JsonData>(json);
+
+            return data?.Form;
+        }
+
+        public static void GenerateFormElements(Form form, StackLayout layout)
+        {
+            if (form == null || layout == null) return;
+
+            foreach (var item in form.Items)
+            {
+                item.AddToLayout(layout);
+            }
+        }
+    }
+}
--- src/ClientApp/ClientApp/Views/FormPage.xaml.cs
+++ src/ClientApp/ClientApp/Views/FormPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using ClientApp.Models;
+using ClientApp.Services;
 using ClientApp.ViewModels;
 using Newtonsoft.Json;
 using Xamarin.Forms;
@@ -8,36 +9,14 @@ namespace ClientApp.Views
 {
     public partial class FormPage : ContentPage
     {
-        public Form Form { get; set; }
-
         public FormPage()
         {
             InitializeComponent();
-            LoadForm(Properties.Resources.Form2);
-            GenerateFormElements(Form);
+            FormService.LoadForm(Properties.Resources.Form2);
+            FormService.GenerateFormElements(Form, FormStack);
             BindingContext = new FormViewModel();
         }
 
-        public void LoadForm(byte[] bytes)
-        {
-            using var stream = new MemoryStream(bytes);
-            using var reader = new StreamReader(stream);
-
-            var json = reader.ReadToEnd();
-
-            var data = JsonConvert.DeserializeObject<JsonData>(json);
-            
-            Form = data?.Form;
-        }
-
-        private void GenerateFormElements(Form form)
-        {
-            if (form == null) return;
-
-            foreach (var item in form.Items)
-            {
-                item.AddToLayout(FormStack);
-            }
-        }
+        public Form Form { get; set; }
     }
 }
