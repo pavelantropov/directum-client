5ce21f3c56d13df80fa27559861eac96cacc271c
Pavel Antropov <p.m.antropov@gmail.com>
2022-05-15 21:26:44 +0400

    Develop deserialization logic

---
+++ src/ClientApp/ClientApp/Helpers/FormElementConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ClientApp.Models;
+using ClientApp.Models.Abstractions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ClientApp.Helpers
+{
+    public class FormElementConverter : JsonConverter
+    {
+        public override bool CanWrite => false;
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var jo = JObject.Load(reader);
+            
+            var type = (string) jo["type"];
+
+            IFormElement element;
+
+            switch (type)
+            {
+                case "filler":
+                    element = new Filler();
+                    break;
+                case "text":
+                    element = new Text();
+                    break;
+                case "textarea":
+                    element = new Textarea();
+                    break;
+                case "checkbox":
+                    element = new Checkbox();
+                    break;
+                case "select":
+                    element = new Select();
+                    break;
+                case "radio":
+                    element = new Radio();
+                    break;
+                case "button":
+                    element = new Button();
+                    break;
+                default:
+                    return null;
+            }
+
+            serializer.Populate(jo.CreateReader(), element);
+
+            return element;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(IFormElement).IsAssignableFrom(objectType);
+        }
+    }
+}
--- src/ClientApp/ClientApp/Views/FormPage.xaml.cs
+++ src/ClientApp/ClientApp/Views/FormPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using ClientApp.Models;
+﻿using System.IO;
+using ClientApp.Models;
 using ClientApp.ViewModels;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace ClientApp.Views
@@ -11,7 +13,22 @@ namespace ClientApp.Views
         public FormPage()
         {
             InitializeComponent();
+            LoadDynamicFields();
             BindingContext = new FormViewModel();
+        }
+
+        public void LoadDynamicFields()
+        {
+            if (!(Properties.Resources.ResourceManager.GetObject("Form1") is byte[] fileBytes)) return;
+
+            using var stream = new MemoryStream(fileBytes);
+            using var reader = new StreamReader(stream);
+
+            var json = reader.ReadToEnd();
+
+            var data = JsonConvert.DeserializeObject<JsonData>(json);
+            
+            Form = data?.Form;
         }
     }
 }
