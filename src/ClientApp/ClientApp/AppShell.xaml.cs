787dfb201ce89205780ccd21e87ba16ba1d0ffa5
Pavel Antropov <p.m.antropov@gmail.com>
2022-05-15 19:17:02 +0400

    Create a page for the form

--- src/ClientApp/ClientApp/AppShell.xaml.cs
+++ src/ClientApp/ClientApp/AppShell.xaml.cs
@@ -13,6 +13,7 @@ namespace ClientApp
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(FormPage), typeof(FormPage));
         }
 
     }
---
+++ src/ClientApp/ClientApp/Views/FormPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ClientApp.Models;
+using ClientApp.ViewModels;
+using Xamarin.Forms;
+
+namespace ClientApp.Views
+{
+    public partial class FormPage : ContentPage
+    {
+        public Form Form { get; set; }
+
+        public FormPage()
+        {
+            InitializeComponent();
+            BindingContext = new FormViewModel();
+        }
+    }
+}
